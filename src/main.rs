4d7059bdcb895b94383d7188c8c8eb83d6c9e4d1
hydrostaticcog <hydro@hydrostaticcog.me>
2022-01-01 12:18:28 -0600

    update the snowflake and add node id

--- src/main.rs
+++ src/main.rs
@@ -17,16 +17,17 @@ enum ModelType {
 
 fn parse(snowflake: u128) {
     println!("Snowflake {}", snowflake);
-    let fc_offset = 1_577_836_800_000;
+    let fc_offset = 1_640_995_200_000;
 
     // Finds timestamp when snowflake was created and converts to datetime
     let ts_offset_ms = snowflake >> 64;
+    let ts_offset = (ts_offset_ms / 1000) as i64;
     let ts_ms = ts_offset_ms + fc_offset;
     let timestamp: i64 = (ts_ms / 1000) as i64;
     let nt = NaiveDateTime::from_timestamp(timestamp, 0);
     let dt: DateTime<Utc> = DateTime::from_utc(nt, Utc);
     let res = dt.format("%Y-%m-%d %H:%M:%S");
-    println!("Created at {} ({})", res, timestamp);
+    println!("Created at {} ({}), offset {}", res, timestamp, ts_offset);
 
     // Finds the model type
     let model_type = (snowflake >> 56 & 0b11111111) as u8;
@@ -40,6 +41,10 @@ fn parse(snowflake: u128) {
     // Finds the API version
     let api_version = snowflake >> 34 & 0b11111111;
     println!("API Version - {}", api_version);
+
+    // Finds the node ID
+    let node_id = snowflake >> 16 & 0b1111111111111111;
+    println!("Node ID - {}", node_id);
 
     // Finds unset bits
     let unset = snowflake & 0b1111111111111111;
